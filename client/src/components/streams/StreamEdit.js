9630ca8bea8d494220f68db3b6d6763ef708ffd6
Deyán G. Sabogal <deyanfabricioguacaransabogal@MacBook-Pro-de-Deyan-Fabricio-Guacaran-Sabogal.local>
2020-07-22 17:15:06 +0100

    Creating StreamEdit Component and refactor form with StreamForm Component

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>;
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    return <div>{this.props.stream.title}</div>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  // ownProps => props from component
+  return {
+    stream: state.streams[ownProps.match.params.id],
+  };
 };
 
-export default StreamEdit;
+export default connect(mapStateToProps, { fetchStream: fetchStream })(
+  StreamEdit
+);
