635ce732272c89b08245ad31c40a6b55649b0d1b
Deyán G. Sabogal <deyanfabricioguacaransabogal@MacBook-Pro-de-Deyan-Fabricio-Guacaran-Sabogal.local>
2020-07-22 11:44:13 +0100

    Add conditional show Edit and Delete Buttons on StreamList Component

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -23,9 +23,12 @@ export const signOut = () => {
 };
 
 export const createStream = (formValues) => {
-  return async (dispatch) => {
-    const response = await streams.post("/streams", formValues);
+  return async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    const response = await streams.post("/streams", { ...formValues, userId });
     dispatch({ type: CREATE_STREAM, payload: response.data });
+
+    // Do some programmatic navigation to get the user back to the root router
   };
 };
 
