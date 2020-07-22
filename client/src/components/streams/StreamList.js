1e9400b0dde319a722e765e83bf75edcbcfcc87a
Deyán G. Sabogal <deyanfabricioguacaransabogal@MacBook-Pro-de-Deyan-Fabricio-Guacaran-Sabogal.local>
2020-07-22 12:08:54 +0100

    Create a Browser History Object

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -11,7 +11,7 @@ class StreamList extends React.Component {
   renderAdmin(stream) {
     if (stream.userId === this.props.currentUserId) {
       return (
-        <div class="right floated content">
+        <div className="right floated content">
           <button className="ui button primary">Edit</button>
           <button className="ui button negative">Delete</button>
         </div>
