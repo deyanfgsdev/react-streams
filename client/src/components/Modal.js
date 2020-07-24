f46c370794b6cf5904f84c93e62554880b5a4786
Deyán G. Sabogal <deyanfabricioguacaransabogal@MacBook-Pro-de-Deyan-Fabricio-Guacaran-Sabogal.local>
2020-07-24 12:34:10 +0100

    Make Modal Component Reusable

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,25 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import history from "../history";
 
 const Modal = (props) => {
   return ReactDOM.createPortal(
-    <div
-      className="ui dimmer modals visible active"
-      onClick={() => history.push("/")}
-    >
+    <div className="ui dimmer modals visible active" onClick={props.onDismiss}>
       <div
         className="ui stantard modal visible active"
         onClick={(event) => event.stopPropagation()}
       >
-        <div className="header">Delete Stream</div>
-        <div className="content">
-          Are you sure you want to delete this stream?
-        </div>
-        <div className="actions">
-          <div className="ui primary button">Delete</div>
-          <div className="ui button">Cancel</div>
-        </div>
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
       </div>
     </div>,
     document.querySelector("#modal")
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Modal from "../Modal";
+import history from "../../history";
 
 const StreamDelete = () => {
+  const actions = (
+    <React.Fragment>
+      <div className="ui button negative">Delete</div>
+      <div className="ui button">Cancel</div>
+    </React.Fragment>
+  );
+
   return (
     <div>
       StreamDelete
-      <Modal />
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+        onDismiss={() => history.push("/")}
+      />
     </div>
   );
 };
